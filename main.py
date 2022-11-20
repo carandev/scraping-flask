1894ea568c2497624abc807b2fa002fb8d1c663b
Carlos Gomez <carlosandres0741@gmail.com>
2022-11-20 12:17:47 -0500

    Added OLX scraping and fixed styles

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 from scrap.main import search_products
 
 app = Flask(__name__)
@@ -7,12 +7,20 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/search/', methods=['POST'])
-def search_word():
-    word = request.form['word']
-    products = search_products(word)
 
-    return render_template('products.html', products = products)
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/search/<string:word>', methods=['GET'])
+def search_word(word):
+    if (len(word) == 0):
+        products = search_products()
+    else:
+        products = search_products(word)
+
+    return render_template('products.html', products = products, len=len, range=range, int=int)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- scrap/main.py
+++ scrap/main.py
@@ -1,8 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 
-
 def search_products(word_search = 'teclado'):
+    products = []
+
+    mercado_libre(word_search, products)
+    olx(word_search, products)
+
+    return products
+
+def mercado_libre(word_search = 'teclado', products = []):
 
     url = f'https://listado.mercadolibre.com.co/{word_search}'
 
@@ -11,8 +18,7 @@ def search_products(word_search = 'teclado'):
     html = BeautifulSoup(request.text, 'html.parser')
 
     product_containers = html.find_all('li', attrs={'class': 'ui-search-layout__item'})
-
-    products = []
+    shop = 'mercadolibre'
 
     for product_container in product_containers:
         title = product_container.findAll('a')[1].get_text()
@@ -20,6 +26,37 @@ def search_products(word_search = 'teclado'):
         price = product_container.find('span', attrs={'class': 'price-tag-fraction'}).get_text()
         img_url = product_container.find('img')['data-src']
 
-        products.append({'title': title, 'url': url, 'price': price, 'img_url': img_url})
+        products.append({
+            'title': title,
+            'url': url,
+            'price': price,
+            'img_url': img_url,
+            'shop': shop
+        })
 
-    return products
+def olx(word_search = 'teclado', products = []):
+
+    BASE_URL = 'https://www.olx.com.co'
+    url = f'{BASE_URL}/items/q-{word_search}'
+
+    request = requests.get(url)
+
+    html = BeautifulSoup(request.text, 'html.parser')
+
+    product_containers = html.find_all('li', attrs={'data-aut-id': 'itemBox'})
+
+    shop = 'olx'
+
+    for product in product_containers:
+        product_title = product.find('span', {'data-aut-id': 'itemTitle'}).get_text()
+        product_url = f"{BASE_URL}{product.find('a')['href']}"
+        product_price = product.find('span', {'data-aut-id': 'itemPrice'}).get_text()
+        product_img_url = product.find('img')['src']
+
+        products.append({
+            'title': product_title,
+            'url': product_url,
+            'price': product_price,
+            'img_url': product_img_url,
+            'shop': shop
+        })
